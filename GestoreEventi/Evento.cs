937e6fbc95e142a8471e7c9be742978b46a444ab
ZahidRamirez00 <zahidramirez.00@gmail.com>
2022-12-13 10:36:27 +0100

    Creazione del costruttore della classe Evento

--- GestoreEventi/Evento.cs
+++ GestoreEventi/Evento.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace GestoreEventi
 {
-    internal class Evento
+    public class Evento
     {
+        // PROPRIETA / ATTRIBUTI
+        private string Titolo;
+        private DateTime Data;
+        private int CapacitaMassima;
+        private int PostiPrenotati;
+        // STATO
+        // COSTRUTTORI
+        public Evento(string titolo, DateTime data, int capacitaMassima, int postiPrenotati) 
+        {
+            this.Titolo = titolo;
+            this.Data = data;
+            this.CapacitaMassima = capacitaMassima;
+            this.PostiPrenotati = postiPrenotati;
+        }
+        // GETTERS
+
+        // SETTERS
+        // METODI PUBBLICI
+        // METODI PRIVATI
     }
 }

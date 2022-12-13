50accdfb44d71caa992895be68b14eca73627178
ZahidRamirez00 <zahidramirez.00@gmail.com>
2022-12-13 11:36:30 +0100

    Creazione del metodo PrenotaPosti

--- GestoreEventi/Evento.cs
+++ GestoreEventi/Evento.cs
@@ -15,7 +15,7 @@ namespace GestoreEventi
         private int PostiPrenotati;
         // STATO
         // COSTRUTTORI
-        public Evento(string titolo, DateTime data, int capacitaMassima, int postiPrenotati) 
+        public Evento(string titolo, DateTime data, int capacitaMassima, int postiPrenotati = 0) 
         {
             this.Titolo = titolo;
             this.Data = data;
@@ -23,9 +23,42 @@ namespace GestoreEventi
             this.PostiPrenotati = postiPrenotati;
         }
         // GETTERS
+        public string GetTitolo()
+        {
+            return this.Titolo;
+        }
 
+        public DateTime GetData()
+        {
+            return this.Data;
+        }
+
+        public int GetCapacitaMassima()
+        {
+            return this.CapacitaMassima;
+        }
         // SETTERS
+        public void SetTitolo(string titolo)
+        {
+            this.Titolo = titolo;
+        }
+
+        public void SetData(DateTime data)
+        {
+            this.Data = data;
+        }
         // METODI PUBBLICI
+        public void PrenotaPosti()
+        {
+            if(this.PostiPrenotati < this.CapacitaMassima || this.PostiPrenotati >= 0)
+            {
+                this.PostiPrenotati++;
+            }
+            else
+            {
+                Console.WriteLine("Il valore non può essere minore di 0 o maggiore della capacità massima (" + this.CapacitaMassima + ")");
+            }
+        }
         // METODI PRIVATI
     }
 }
